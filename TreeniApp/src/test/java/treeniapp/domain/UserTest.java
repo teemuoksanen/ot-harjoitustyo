c1e4571a287f82f3b9933535ddef5fa06b628d03
Teemu Oksanen <github@teemuoksanen.fi>
2020-03-31 22:51:49 +0300

    Testien korjaus

--- TreeniApp/src/test/java/treeniapp/domain/UserTest.java
+++ TreeniApp/src/test/java/treeniapp/domain/UserTest.java
@@ -1,9 +1,12 @@
 
 package treeniapp.domain;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.Before;
 
 public class UserTest {
     
@@ -14,6 +17,24 @@ public class UserTest {
     public void setUp() {
         userDao = new FakeUserDao();
         service = new TreeniAppService(userDao);     
+    }
+    
+    @Test
+    public void loginSuccessfull() {
+        boolean result = service.login("testi");
+        User loggedInUser = service.getLoggedInUser();
+        
+        assertTrue(result);
+        assertEquals("testi", loggedInUser.getUsername());
+        assertEquals("Teemu Testi", loggedInUser.getName());
+    }
+    
+    @Test
+    public void logoutSuccessfull() {
+        service.login("testi");
+        service.logout();
+        
+        assertEquals(null, service.getLoggedInUser());
     }
     
     @Test
